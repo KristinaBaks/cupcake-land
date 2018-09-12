b005930bd2ccb591e1147d457cd68b1d76a06efe
Kristina <kristinabaksytee@gmail.com>
2018-09-12 17:43:50 +0300

    edit nr1

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -2,18 +2,23 @@
 //------- NAVIGATION ------------
 const navIcon = document.querySelector('.nav-icon');
 const nav = document.querySelector('.nav');
+const navList = document.querySelector('.nav__list');
 
 toggleNavigation();
 
 function toggleNavigation() {
-    navIcon.addEventListener('click', function() {
+    navIcon.addEventListener('click', function(){
         if(navIcon.className == 'nav-icon') {
             navIcon.className = 'nav-icon close-icon';
-            nav.style.display = "block";
+            nav.style.display = 'block';
         } else {
             navIcon.className = 'nav-icon';
-            nav.style.display = "none";
+            nav.style.display = 'none';
         }
+    })
+    navList.addEventListener('click', function(){
+        navIcon.className = 'nav-icon';
+        nav.style.display = 'none';
     })
 }
 //------- SLIDESHOW ------------
@@ -23,15 +28,15 @@ showSlides();
 
 function showSlides() {
     let i;
-    const slides = document.querySelectorAll(".slideshow-box__slides");
+    const slides = document.querySelectorAll('.slideshow-box__slides');
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        slides[i].style.display = 'none';
     }
     slideIndex++;
     if (slideIndex > slides.length) {
         slideIndex = 1;
     }
-    slides[slideIndex-1].style.display = "block"; 
+    slides[slideIndex-1].style.display = 'block'; 
     setTimeout(showSlides, 2000);
 }
 
