2574005694864b0a81961325ef89333213274cab
Kristina <kristinabaksytee@gmail.com>
2018-09-12 16:38:36 +0300

    first commit

---
+++ docs/assets/js/main.js
@@ -0,0 +1,37 @@
+
+//------- NAVIGATION ------------
+const navIcon = document.querySelector('.nav-icon');
+const nav = document.querySelector('.nav');
+
+toggleNavigation();
+
+function toggleNavigation() {
+    navIcon.addEventListener('click', function() {
+        if(navIcon.className == 'nav-icon') {
+            navIcon.className = 'nav-icon close-icon';
+            nav.style.display = "block";
+        } else {
+            navIcon.className = 'nav-icon';
+            nav.style.display = "none";
+        }
+    })
+}
+//------- SLIDESHOW ------------
+let slideIndex = 0;
+
+showSlides();
+
+function showSlides() {
+    let i;
+    const slides = document.querySelectorAll(".slideshow-box__slides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 2000);
+}
+
